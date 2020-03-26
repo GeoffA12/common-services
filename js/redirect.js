098aebfb60c7a65d7da25a2df424de4c1e8633c2
Jeffrey Ng <komoto415@gmail.com>
2020-03-26 00:58:15 -0500

    removing dumb console.log

--- js/redirect.js
+++ js/redirect.js
@@ -1,5 +1,4 @@
 let urlPrefix = (window.location.href.includes("demand")) ? "demand" : "supply";
-console.log(urlPrefix);
 let goToLogin = () => {
     // console.log('login pressed')
     window.location.assign(`https://${urlPrefix}.team22.softwareengineeringii.com/common-services/login/login.html`);
