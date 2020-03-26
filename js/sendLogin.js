2e03dc6d0f9291308068d2dd24de4f362a76a24c
Jeffrey Ng <komoto415@gmail.com>
2020-03-25 19:57:41 -0500

    spacing improvements

--- js/sendLogin.js
+++ js/sendLogin.js
@@ -2,6 +2,7 @@ let sendPostRequest = () => {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     var urlPrefix = (window.location.href.includes("demand")) ? "demand" : "supply";
+
     const login = {
         "username": username,
         "password": password,
@@ -18,7 +19,9 @@ let sendPostRequest = () => {
         body: JSON.stringify(login)
     }
     // console.log(payload);
+    
     var url = `https://${urlPrefix}.team22.softwareengineeringii.com/loginHandler`;
+    // console.log(url);
 
     fetch(url, payload).then(function (response) {
         // console.log(response.status);
@@ -26,10 +29,9 @@ let sendPostRequest = () => {
             url = `https://${urlPrefix}.team22.softwareengineeringii.com/${urlPrefix}-front-end/dashboard/dashboard.html`
             window.location.assign(url);
         } else {
-            document.getElementById('loginError').innerHTML = 'Invalid login or password'
+            document.getElementById('error').innerHTML = 'Invalid login or password'
         }
     }).catch(function (error) {
         console.error(error)
-
     });
 }
