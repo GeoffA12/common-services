fc4167f32029237e3852b5dd0e50edaa8e99400d
Jeffrey Ng <komoto415@gmail.com>
2020-03-26 00:57:53 -0500

    changed session storage to align with new var

--- js/sendRegister.js
+++ js/sendRegister.js
@@ -1,14 +1,20 @@
 let sendRegisrationForm = () => {
+    let firstName = document.getElementById('firstname').value;
+    let lastName = document.getElementById('lastname').value;
+    let phoneNumber = document.getElementById('phonenumber').value; 
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
     var urlPrefix = (window.location.href.includes("demand")) ? "demand" : "supply";
 
     const registration = {
+        "firstname": firstName,
+        "lastname": lastName,
+        "phoneNumber": phoneNumber,
         "email": email,
         "password": password,
         "cloud": urlPrefix
     }
-    sessionStorage.setItem('username', username);
+    sessionStorage.setItem('username', email);
 
     const payload = {
         method: 'POST',
