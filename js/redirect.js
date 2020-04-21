cf3487ed927f09476567943c9e2ef721d24f846e
Jeffrey Ng <komoto415@gmail.com>
2020-04-20 21:12:25 -0500

    clearning local storage of username to sign user out

--- js/redirect.js
+++ js/redirect.js
@@ -9,3 +9,7 @@ let goToLogin = () => {
 let goToRegister = () => {
     window.location.assign(`https://${urlPrefix}.team22.softwareengineeringii.com/common-services/register/register.html`);
 }
+let signOut = () => {
+    localStorage.removeItem('username');
+    window.location.assign(`https://${urlPrefix}.team22.softwareengineeringii.com`);
+}
