acf28cd1cc34d661b19147cf922ada03dc65f8c7
Jeffrey Ng <komoto415@gmail.com>
2020-04-27 14:37:56 -0500

    something wrong with using let

--- js/redirect.js
+++ js/redirect.js
@@ -1,19 +1,19 @@
-let urlPrefix = (window.location.href.includes("demand")) ? "demand" : "supply";
-let goToLogin = () => {
+var urlPrefix = (window.location.href.includes("demand")) ? "demand" : "supply";
+var goToLogin = () => {
     if (localStorage.getItem('username') != null) {
         window.location.assign(`https://${urlPrefix}.team22.softwareengineeringii.com/${urlPrefix}-front-end/dashboard/dashboard.html`);
     } else {
         window.location.assign(`https://${urlPrefix}.team22.softwareengineeringii.com/common-services/login/login.html`);
     }
 }
-let goToRegister = () => {
+var goToRegister = () => {
     window.location.assign(`https://${urlPrefix}.team22.softwareengineeringii.com/common-services/register/register.html`);
 }
-let goToLanding = () => {
+var goToLanding = () => {
     window.location.assign(`https://${urlPrefix}.team22.softwareengineeringii.com`);
 }
 
-let signOut = () => {
+var signOut = () => {
     localStorage.removeItem('username');
     window.location.assign(`https://${urlPrefix}.team22.softwareengineeringii.com/common-services/confirmsignout.html`);
 }
